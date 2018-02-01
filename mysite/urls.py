0665dda912e51171e32c07167500a65ccd2ab110
Keangping <pingandhispong@hotmail.com>
2018-02-01 15:42:30 +0100

    add static file

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'mysite'
+
 urlpatterns = [
+	# /
 	url(r'^$', views.index, name='index'),
+
+	# /omoss
+	url(r'^omoss/$', views.contact, name='contact'),
+
+	
+
 ]
